bf6876b6baf03f5d8d577796c86a0c9ec3030968
Pradyot Srivastava <pradyotsrivastava93@gmail.com>
2025-07-29 21:41:00 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ import routes from "./routes/index.js";
 dotenv.config();
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL || "*",
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
