be4402b009f322306c0314f3350eb97b3b8384fd
Pradyot Srivastava <pradyotsrivastava93@gmail.com>
2025-07-29 22:19:24 +0530

    vercel

--- client/src/auth/Register.jsx
+++ client/src/auth/Register.jsx
@@ -57,7 +57,7 @@ const Register = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+      <div className="bg-white px-8 py-2 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
 
         <Formik
